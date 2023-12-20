d951047a298f58bd7fce708faf8b111741e3a55c
efenocchi <emanuele.fenocchi@gmail.com>
2023-12-20 22:53:59 +0100

    files commit

---
+++ global_variable.py
@@ -0,0 +1,26 @@
+# pylint: disable=locally-disabled, multiple-statements, fixme, line-too-long, missing-module-docstring, missing-function-docstring
+
+VECTOR_STORE_PATH_COMPLETE_SEQUENTIALLY = (
+    "hub://manufe/pill_image_description_LLAMAINDEX_BM25_COMPLETE-SEQUENTIALLY"
+)
+
+VECTOR_STORE_PATH_BASELINE = (
+    "hub://manufe/pills_image_description_LLAMAINDEX_BM25_BASELINE"
+)
+
+VECTOR_STORE_PATH_HYBRID = "hub://manufe/pill_image_description_Hybrid_Fusion_Retriever"
+
+
+PILLS_JSON_FILE_CLEANED = "pills_info_cleaned.json"
+PILLS_JSON_FILE_CLEANED_2 = "pills_info_cleaned2.json"
+
+QUERY = "What is the Abilify pill used for?"
+
+VECTOR_STORE_PATH_IMAGES_MASKED = (
+    "hub://manufe/pill_images_vector_store_IMG_TEXT_MASKED"
+)
+VECTOR_STORE_PATH_IMAGES_NORMAL = (
+    "hub://manufe/pill_images_vector_store_IMG_TEXT_NORMAL"
+)
+
+VECTOR_STORE_PATH_DESCRIPTION = "hub://manufe/pill_image_description_vector_store2"
